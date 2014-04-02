ad73cc0d24f4fd00ccd3f85135c787f2f656435d
Jason Ray <jasonray@jayray.net>
2014-04-01 23:19:25 -0400

    add add/subtract methods

--- app.js
+++ app.js
@@ -3,15 +3,40 @@ var restify = require('restify');
 function echo(req, res, next) {
 	console.log('req: [%s]', req);
 	console.log('res: [%s]', res);
-	var message = req.params.message;
+	var message = req.query.m;
 	console.log('message: %s', message);
 	res.send(message);
 	next();
 }
 
+function add(req, res, next) {
+	var x = parseInt(req.query.x);
+	var y = parseInt(req.query.y);
+	var sum = x + y;
+	console.log('%s + %s = %s',x ,y, sum);
+	res.send(sum.toString());
+	next();
+}
+
+function subtract(req, res, next) {
+	var x = parseInt(req.query.x);
+	var y = parseInt(req.query.y);
+	var diff = x - y;
+	console.log('%s - %s = %s',x ,y, diff);
+	res.send(diff.toString());
+	next();
+}
+
 var server = restify.createServer();
-server.get('/echo/:message', echo);
-server.head('/echo/:message', echo);
+server.use(restify.queryParser());
+
+server.get('/add', add);
+server.get('/subtract', subtract);
+
+server.get('/echo', echo);
+server.head('/echo', echo);
+//server.get('/echo/:message', echo);
+
 
 server.listen(8080, function() {
 	console.log('%s listening at %s', server.name, server.url);
