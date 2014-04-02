a7710fa3889ef7e2fed4d1f4c818378f7ad1cb40
Jason Ray <jasonray@jayray.net>
2014-04-01 23:33:51 -0400

    cleanup

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 var restify = require('restify');
 
+var server = restify.createServer();
+server.use(restify.queryParser());
+
+server.get('/add', add);
+server.get('/subtract', subtract);
+
+server.get('/echo', echo);
+server.head('/echo', echo);
+server.get('/echo/:message', echo);
+server.head('/echo/:message', echo);
+
+
+server.listen(8080, function() {
+	console.log('%s listening at %s', server.name, server.url);
+});
+
 function echo(req, res, next) {
 	console.log('req: [%s]', req);
 	console.log('res: [%s]', res);
-	var message = req.query.m;
+	var message = req.params.message;
+	if (message==null) {
+		message=req.query.m;
+	}
 	console.log('message: %s', message);
 	res.send(message);
 	next();
@@ -26,18 +45,3 @@ function subtract(req, res, next) {
 	res.send(diff.toString());
 	next();
 }
-
-var server = restify.createServer();
-server.use(restify.queryParser());
-
-server.get('/add', add);
-server.get('/subtract', subtract);
-
-server.get('/echo', echo);
-server.head('/echo', echo);
-//server.get('/echo/:message', echo);
-
-
-server.listen(8080, function() {
-	console.log('%s listening at %s', server.name, server.url);
-});
